d0637cf5cb185bd42b338db2011bd10cdd04a8e2
Seth Nielsen <sethmnielsen@gmail.com>
2017-09-06 13:33:14 -0600

    deleted /devel and /build

--- mapping_ws/devel/share/robot_localization/cmake/robot_localization-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(robot_localization_MESSAGE_FILES "")
-set(robot_localization_SERVICE_FILES "/home/seth/mapping_ws/src/robot_localization/srv/GetState.srv;/home/seth/mapping_ws/src/robot_localization/srv/SetDatum.srv;/home/seth/mapping_ws/src/robot_localization/srv/SetPose.srv")
